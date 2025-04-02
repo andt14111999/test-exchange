6891d61d73c83656f76967ffa52a880aa900df01
dungnt97 <quyle15697@gmail.com>
2025-04-02 23:19:03 +0700

    Fix admin notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -9,6 +9,24 @@ class Notification < ApplicationRecord
 
   after_create :broadcast_notification
 
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[
+      content
+      created_at
+      delivered
+      id
+      notification_type
+      read
+      title
+      updated_at
+      user_id
+    ]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[user]
+  end
+
   private
 
   def broadcast_notification
