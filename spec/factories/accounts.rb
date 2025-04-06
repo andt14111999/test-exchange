c5bdc3b63cafaf5f74660a97fac9179d44639e1b
dungnt97 <quyle15697@gmail.com>
2025-04-06 09:35:57 +0700

    test: improve categorized account spec with better RSpec practices

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :account do
-    account_type { 'wallet' }
+    sequence(:address) { |n| "address_#{n}" }
+    account_type { 'main' }
     coin_currency { 'BTC' }
     layer { 'bitcoin' }
     user
