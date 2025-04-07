f5ed846b3e95d3cb5691a6bcb507405bc057ac9d
dungnt97 <quyle15697@gmail.com>
2025-04-08 03:30:08 +0700

    Fix CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ SimpleCov.start 'rails' do
   add_group 'Helpers', 'app/helpers'
 
   minimum_coverage 90
-  minimum_coverage_by_file 80
+  minimum_coverage_by_file 90
 end
 
 RSpec.configure do |config|
