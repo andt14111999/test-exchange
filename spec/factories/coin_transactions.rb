0346567ef49781e7fbda6228193bfa3820ec8465
dungnt97 <quyle15697@gmail.com>
2025-04-08 02:36:57 +0700

    refactor: remove debug puts statements from coin withdrawal operations spec

--- spec/factories/coin_transactions.rb
+++ spec/factories/coin_transactions.rb
@@ -5,5 +5,10 @@ FactoryBot.define do
     transaction_type { 'transfer' }
     association :coin_account, factory: [ :coin_account, :main ]
     operation { nil }
+
+    trait :with_withdrawal_operation do
+      transaction_type { 'withdrawal' }
+      association :operation, factory: :coin_withdrawal_operation
+    end
   end
 end
