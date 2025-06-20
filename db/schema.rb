b225c3fd42c3482f9c46c6dfad6651443cff02f2
dungnt97 <quyle15697@gmail.com>
2025-06-20 11:13:48 +0700

    Update migration coin setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_214025) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_20_035958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
