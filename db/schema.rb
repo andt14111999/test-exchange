9f331abee49cb46762513b7dda0b358953da47d5
ngocbv <ngocbv1994@gmail.com>
2025-06-24 10:29:53 +0700

    Add process_message field to KafkaEvent and enhance logging
    - Added a new text column `process_message` to the KafkaEvent model to store detailed processing messages.
    - Updated ActiveAdmin to include the `process_message` field in the KafkaEvent index and show pages.
    - Enhanced the CoinWithdrawalHandler to log success and failure messages to the `process_message` field during status updates.
    - Implemented tests for the new `update_kafka_event_message` method to ensure correct logging behavior and message appending.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_23_052503) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_24_032244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -538,6 +538,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_23_052503) do
     t.string "error_message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "process_message"
     t.index ["event_id", "topic_name"], name: "index_kafka_events_on_event_id_and_topic_name", unique: true
     t.index ["status"], name: "index_kafka_events_on_status"
   end
